12ae265e10d6486d2e13f10b6b3f6df064546d44
nguyen <nguyen.nc2710@gmail.com>
2023-07-02 16:20:33 +0700

    add wezterm config file

---
+++ .wezterm.lua
@@ -0,0 +1,35 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Hardcore'
+
+config.max_fps = 100
+config.animation_fps = 100
+config.window_decorations = "RESIZE"
+config.font_size = 13.0
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+config.native_macos_fullscreen_mode = true
+
+-- and finally, return the configuration to wezterm
+return config
+
