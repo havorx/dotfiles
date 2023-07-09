33cd4d55da76c3de1df2ab8e97bb33da2e48c71f
nguyen <nguyen.nc2710@gmail.com>
2023-07-10 00:41:27 +0700

    wezterm: add default border padding config

--- .wezterm.lua
+++ .wezterm.lua
@@ -32,5 +32,13 @@ config.native_macos_fullscreen_mode = true
 
 config.line_height = 1.2
 
+-- wezterm default border padding
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 -- and finally, return the configuration to wezterm
 return config
