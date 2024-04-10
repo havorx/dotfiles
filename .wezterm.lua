90b5e4af22d0e4fe99de4ffc9652e9142dec4102
nguyen <nguyen.nc2710@gmail.com>
2024-04-11 02:08:35 +0700

    config: update wezterm config for latest preferences

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -15,32 +15,68 @@ end
 -- For example, changing the color scheme:
 config.color_scheme = "carbonfox"
 config.colors = {
-  background = "black",
+	background = "black",
 }
 
 config.max_fps = 100
 config.animation_fps = 100
 config.window_decorations = "RESIZE"
-config.font_size = 13.0
+config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
 
 config.keys = {
-  {
-    key = "n",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "P",
+		mods = "CTRL",
+		action = wezterm.action({
+			QuickSelectArgs = {
+				patterns = {
+					"https?://\\S+",
+				},
+				action = wezterm.action_callback(function(window, pane)
+					local url = window:get_selection_text_for_pane(pane)
+					wezterm.log_info("opening: " .. url)
+					wezterm.open_with(url)
+				end),
+			},
+		}),
+	},
 }
+
 config.native_macos_fullscreen_mode = true
 
-config.line_height = 1.2
+-- config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
 
--- wezterm default border padding
+-- config.front_end = "WebGpu"
+
+-- config.webgpu_power_preference = "HighPerformance"
+
+-- config.line_height = 1.0
+
+-- Spawn a powershell shell in login mode
+-- config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
+
+-- start wezterm in wsl
+-- config.default_domain = "WSL:Ubuntu"
+-- config.launch_menu = {
+-- 	{
+-- 		label = "pwsh",
+-- 		args = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\pwsh.exe", "-nologo" },
+-- 		domain = { DomainName = "local" },
+-- 		cwd = "C:\\Users\\havorx",
+-- 	},
+-- }
+
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 3,
+	right = 0,
+	-- top = 0,
+	bottom = 0,
 }
 
 -- and finally, return the configuration to wezterm
