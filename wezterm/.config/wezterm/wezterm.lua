242dd3d16af2d2d0ea869afaf921ebe9f9845b68
nguyen <nguyen.nc2710@gmail.com>
2026-06-30 16:33:06 +0700

    refactor(wezterm): migrate to XDG, add WSL/macOS detection, sync config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "carbonfox"
+config.color_scheme = "Tokyo Night"
 config.colors = {
 	background = "black",
 }
@@ -23,20 +23,21 @@ config.animation_fps = 100
 config.window_decorations = "RESIZE"
 config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
+config.disable_default_mouse_bindings = true
 
 config.keys = {
-	{
-		key = "n",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ToggleFullScreen,
-	},
+	-- {
+	-- 	key = "n",
+	-- 	mods = "SHIFT|CTRL",
+	-- 	action = wezterm.action.ToggleFullScreen,
+	-- },
 	{
 		key = "P",
 		mods = "CTRL",
 		action = wezterm.action({
 			QuickSelectArgs = {
 				patterns = {
-					"https?://\\S+",
+					[[https?://[^\s\])\"'>,;]+(?:\([^\)]*\)[^\s\])\"'>,;]*)*]],
 				},
 				action = wezterm.action_callback(function(window, pane)
 					local url = window:get_selection_text_for_pane(pane)
@@ -48,9 +49,7 @@ config.keys = {
 	},
 }
 
-config.native_macos_fullscreen_mode = true
-
--- config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 
 -- config.front_end = "WebGpu"
 
@@ -58,26 +57,37 @@ config.native_macos_fullscreen_mode = true
 
 -- config.line_height = 1.0
 
--- Spawn a powershell shell in login mode
--- config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
+if wezterm.target_triple:find("windows") then
+	config.default_domain = "WSL:Ubuntu"
+	config.ssh_domains = {
+		{
+			name = 'wsl-ssh',
+			remote_address = 'localhost:22',
+			username = os.getenv('USER'),
+			multiplexing = 'None',
+		},
+	}
+	config.launch_menu = {
+		{
+			label = "pwsh",
+			args = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\pwsh.exe", "-nologo" },
+			domain = { DomainName = "local" },
+			cwd = "C:\\Users\\havorx",
+		},
+	}
+end
 
--- start wezterm in wsl
--- config.default_domain = "WSL:Ubuntu"
--- config.launch_menu = {
--- 	{
--- 		label = "pwsh",
--- 		args = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\pwsh.exe", "-nologo" },
--- 		domain = { DomainName = "local" },
--- 		cwd = "C:\\Users\\havorx",
--- 	},
--- }
+if wezterm.target_triple:find("darwin") then
+	config.native_macos_fullscreen_mode = true
+end
 
 config.window_padding = {
-	left = 3,
+	left = 0,
 	right = 0,
-	-- top = 0,
 	bottom = 0,
 }
+
+config.audible_bell = "Disabled"
 
 -- and finally, return the configuration to wezterm
 return config
